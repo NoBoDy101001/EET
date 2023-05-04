[fix] add cross enc attention mask

python/eet/transformers/modeling_t5.py
@@ -13,7 +13,6 @@ from torch import Tensor
 import torch.nn.functional as F
 from torch.nn.parameter import Parameter
 from typing import Any, Dict, List, Optional, Tuple
-from transformers import T5Model
 from transformers import T5Model, T5ForConditionalGeneration
 from transformers.modeling_outputs import (
     BaseModelOutput,
@@ -160,6 +159,7 @@ class EETT5Block():
         add_residual=True,
         self_past_key_values_length=0,
         position_bias=None,
+        encoder_pre_padding_len=None,
     ):
         batch_size, seq_length = hidden_states.shape[:2]
         real_seq_length = seq_length + self_past_key_values_length        
@@ -195,7 +195,7 @@ class EETT5Block():
 
             cross_attn_out, attn_weights = self.cross_attention(
                 hidden_states=self_attn_out,
-                pre_padding_len=pre_padding_len,
+                pre_padding_len=encoder_pre_padding_len,
                 attention_reweight=attention_reweight,
                 encoder_outputs=encoder_outputs,
                 per_sample_length=per_sample_length,
@@ -321,6 +321,7 @@ class EETT5Decoder():
         normalize_before=True,
         position_bias=None,
         self_past_key_values_length=0,
+        encoder_pre_padding_len=None
     ):
         all_cross_attentions = ()
         hidden_states = self.embed_tokens(input_ids)
@@ -338,6 +339,7 @@ class EETT5Decoder():
                 add_residual=True,
                 position_bias=position_bias,
                 self_past_key_values_length=self_past_key_values_length,
+                encoder_pre_padding_len=encoder_pre_padding_len,
             )
             all_cross_attentions = all_cross_attentions + (attn_weights, )
         hidden_states = self.final_layer_norm(hidden_states)
@@ -419,6 +421,7 @@ class EETT5Model():
             first_pass=first_pass,
             pre_padding_len=decoder_pre_padding_len,
             attention_reweight=attention_reweight,
+            encoder_pre_padding_len=pre_padding_len,
             per_sample_length=per_sample_length,
             head_mask=None,
             reorder_state=self.reorder_state,